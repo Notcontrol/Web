fe9b701dbb87809b2643d84558a83848f731e623
Jiang Lu <18236556108@163.com>
2017-12-21 11:03:22 +0800

    项目初始

---
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by 18236 on 2017/12/21.
+ */
+@Controller
+public class MainController {
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
